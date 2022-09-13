4a6ff62712eeda74ecce11365aaf500c4cc0f8fc
Schott Evan <ets35@zoo.cs.yale.edu>
2022-09-13 11:42:33 -0400

    rough draft MATInit

--- kern/pmm/MATInit/MATInit.c
+++ kern/pmm/MATInit/MATInit.c
@@ -19,11 +19,11 @@
  */
 void pmem_init(unsigned int mbi_addr)
 {
-    unsigned int nps;
-
-    unsigned int highest_phy_address;
-    unsigned int last_index;
     // TODO: Define your local variables here.
+    unsigned int nps;
+    unsigned int highest_phy_address;
+    unsigned int pmm_table_length;
+    
 
     // Calls the lower layer initialization primitive.
     // The parameter mbi_addr should not be used in the further code.
@@ -35,13 +35,9 @@ void pmem_init(unsigned int mbi_addr)
      * Hint: Think of it as the highest address in the ranges of the memory map table,
      *       divided by the page size.
      */
-    // TODO
-
-    last_index = get_size() - 1;
-    highest_phy_address = get_mms(last_index) + get_mml(last_index);
-
-    nps = highest_phy_address/PAGESIZE;
-
+    pmm_table_length = get_size();
+    highest_phy_address = get_mms(pmm_table_length - 1) + get_mml(pmm_table_length - 1);
+    nps = highest_phy_address/PAGESIZE + ((highest_phy_address % PAGESIZE) != 0); // E: Do we need to do ceiling instead of just raw division? 
     set_nps(nps);  // Setting the value computed above to NUM_PAGES.
 
     /**
@@ -68,4 +64,93 @@ void pmem_init(unsigned int mbi_addr)
      *    so in that case, you should consider those pages as unavailable.
      */
     // TODO
+
+    // initialize AT for all the page indices from 0 to NPS - 1
+
+    // All addresses < VM_USERLO (Pages: 0 to VM_USERLO_PI - 1) are reserved by the kernel, ie set its permission to 1.
+    unsigned int page_index = 0;
+    for (;page_index < VM_USERLO_PI; page_index++) {
+        at_set_perm(page_index, 1);
+    }
+
+    // [VM_USERLO, VM_USERHI) ONLY IF the entire page falls into one of the ranges in the memory map table with the permission marked as usable.
+    // The permission should be set to 2 only if there is a range containing the entire page that is marked as available in the memory map table. Otherwise, it should be set to 0.
+    unsigned int pmm_table_index = 0; 
+    unsigned int cur_page_start;
+    unsigned int cur_page_end;
+    for (; page_index < VM_USERHI_PI; page_index++) {
+
+        if (pmm_table_index >= pmm_table_length) {
+            at_set_perm(page_index, 0);
+            continue;
+        }
+
+        cur_page_start = page_index * PAGESIZE;
+        cur_page_end = (page_index + 1) * PAGESIZE - 1;
+        
+        while (pmm_table_index < pmm_table_length && (get_mms(pmm_table_index) + get_mml(pmm_table_index)) < cur_page_start) { // TODO: does this go left -> right? like if at max index will this break
+            page_index++;
+        }  
+
+        if (pmm_table_index >= pmm_table_length) {
+            at_set_perm(page_index, 0);
+            continue; // TODO: Not sure if using "continue" is good etiquitte, should maybe just use if/else
+        }
+
+        unsigned int cur_table_start = get_mms(pmm_table_index);
+        unsigned int cur_table_end = cur_table_start + + get_mml(pmm_table_index);
+        if (cur_table_start <= cur_page_start && is_usable(pmm_table_index)) {
+            
+            // Case current page contained in segment:
+            if (cur_table_end >= cur_page_end) {
+                at_set_perm(page_index, 2);
+                continue;
+            }
+
+            // Otherwise need to loop:
+            cur_page_start = cur_table_end + 1;
+            pmm_table_index++; 
+            while (pmm_table_index < pmm_table_length && get_mms(pmm_table_index) == cur_page_start && is_usable(pmm_table_index)) {
+                cur_table_end = get_mms(pmm_table_index) + get_mml(pmm_table_index);
+                if (cur_table_end >= cur_page_end) {
+                    at_set_perm(page_index, 2); // TODO: need to update table index?
+                    break;
+                }
+                cur_page_start = cur_table_end;
+                pmm_table_index++;
+        }
+
+
+        /*
+        going to have to recalculate:
+        - need to while loop where keep getting further indices that are available
+
+        */
+
+
+        // Idea is going to keep a running index in the pmm table (pmm_table_index), and increase that as increase the page index 
+        /* Lets do a small example: page_index = 3, cur_page_addr = page_index * 4096, pmm_table_index = 0, cur_pmm_table_start = get_mms(pmm_table_index), cur_pmm_table_end = cur_pmm_table_start + get_mml(pmm_table_index) 
+         while (table_end < page_start, table_index < table_length)
+            increase table_index
+            
+            case 0: pmm table finished
+                break
+
+            case 1: don't overlap
+                at_set_perm(page_index, 0);
+                
+            case 2: overlap and not free
+                at_set_perm(page_index, 0);
+
+            case 2: overlap and free   
+                at_set_perm(page_index, 2);
+        */
+
+
+    }
+
+    // all addresses >= VM_USERHI are reserved by the kernel (set its permission to 1) : VM_USERHI_PI to NUM_PAGES - 1
+    for (;page_index < nps - 1; page_index++) {
+        at_set_perm(page_index, 1);
+    }
 }
